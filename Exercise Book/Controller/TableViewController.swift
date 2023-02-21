703a095a4f4a34b94403b4497fbe0dc52838422e
HiImYong99 <good19422@naver.com>
2023-02-21 22:24:21 +0900

    SQLite를 사용하여 CoreData 생성

--- Exercise Book/Controller/TableViewController.swift
+++ Exercise Book/Controller/TableViewController.swift
@@ -6,22 +6,72 @@
 //
 
 import UIKit
+import CoreData
 
 class TableViewController: UITableViewController {
-
-    var arr = [String]()
+    
+    var arr = [Item]()
     var inputText: UITextField?
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        
+        if arr.count == 0 {
+            let alert = UIAlertController(title: "우측 상단 버튼을 눌러 관람한 영화를 추가하세요‼️", message: "", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .default) { (ok) in
+            })
+            self.present(alert, animated: true, completion: nil)
+        }
     }
+    
+    
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return arr.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Main Contents", for: indexPath)
+        let item = arr[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+        return cell
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let item = arr[indexPath.row]
+        
+        item.done = item.done ? false : true
+        saveItems()
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK: - Add Button
+    
     @IBAction func addText(_ sender: UIBarButtonItem) {
-        let alert = UIAlertController(title: "우측 상단 버튼을 터치하여 리스트를 추가하세요‼️", message: "", preferredStyle: .alert)
+        let alert = UIAlertController(title: "관람한 영화 제목을 입력하세요‼️", message: "", preferredStyle: .alert)
         
         let ok = UIAlertAction(title: "OK", style: .default) { (ok) in
-            print((self.inputText?.text!)!)
-            self.arr.append((self.inputText?.text!)!)
-            self.tableView.reloadData()
+            
+            let newItem = Item(context: self.context)
+            newItem.title = self.inputText!.text!
+            newItem.done = false
+            
+            self.arr.append(newItem)
+            self.saveItems()
+    
         }
         
         let cancel = UIAlertAction(title: "cancel", style: .cancel) { (cancel) in
@@ -36,30 +86,30 @@ class TableViewController: UITableViewController {
         }
         self.present(alert, animated: true, completion: nil)
     }
-    // MARK: - Table view data source
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return arr.count
-    }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Main Contents", for: indexPath)
-        cell.textLabel?.text = arr[indexPath.row]
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let cell = tableView.cellForRow(at: indexPath)
+    //MARK: - Model
+
+    func saveItems() {
         
-        if cell?.accessoryType == .checkmark {
-            cell?.accessoryType = .none
-        } else {
-            cell?.accessoryType = .checkmark
+        do{
+           try context.save()
+
+        } catch {
+            print("에러: \(error)")
         }
-        tableView.deselectRow(at: indexPath, animated: true)
-        
-        
-        
+        self.tableView.reloadData()
     }
-    
+
+//    func loadItems() {
+//
+//        do{
+//
+//        } catch {
+//
+//        }
+//    }
 }
+
+
+
